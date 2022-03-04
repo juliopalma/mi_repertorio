d401c88218d40dec38df103c9691f54da7cafc8c
Julio Palma Quezada <j.palma.quezada@gmail.com>
2022-03-04 17:54:14 -0300

    Trabajo de mi repertorio

---
+++ db.js
@@ -0,0 +1,99 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'repertorio',
+    password: '1234',
+    max: 20,
+    min: 2,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
+
+//Insertar
+async function insertar(cancion, artista, tono) {
+    let client;
+    try {
+        client = await pool.connect()
+    } catch (err) {
+        console.log("El error es el siguiente: " + err);
+        return;
+    }
+
+    try {
+        const res = await client.query(
+            "insert into repertorio (cancion, artista, tono) values ($1, $2, $3) returning *", [cancion, artista, tono]
+        )
+        client.release()
+    } catch (err) {
+        console.log("El error es: " + err);
+    }
+}
+
+//Consultar
+async function consultar() {
+    let client
+    try {
+        client = await pool.connect();
+
+    } catch (conn_error) {
+
+        console.log("Error en el cliente");
+        return;
+    }
+
+    let res;
+    try {
+        res = await client.query({ text: `select * from repertorio` });
+    } catch (err) {
+        console.log("El error es: " + err);
+        return;
+    }
+
+    client.release();
+
+    return res.rows;
+};
+
+//Editar
+async function editar(id, cancion, artista, tono) {
+    let client
+    try {
+        client = await pool.connect();
+
+    } catch (conn_error) {
+
+        console.log("Error en el cliente");
+        return;
+    }
+
+    let res;
+    try {
+        res = await client.query({
+            text: "update repertorio set cancion=$2, artista=$3, tono=$4 where id=$1",
+            values: [id, cancion, artista, tono]
+        });
+
+    } catch (err) {
+        console.log("El error es: " + err);
+        return;
+    }
+
+    client.release();
+
+    return res.rows;
+};
+
+//Eliminar
+async function eliminar(id) {
+    const client = await pool.connect()
+    const res = await client.query(
+        "delete from repertorio where id=$1", [id]
+    )
+    client.release()
+    return res.rows
+
+}
+
+module.exports = { insertar, consultar, editar, eliminar }
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { consultar, editar, eliminar, insertar } = require('./db.js');
+const app = express();
+app.use(express.static('static'));
+
+//Insertar
+app.post('/cancion', async(req, res) => {
+    let body = ""
+
+    req.on("data", (data) => {
+        body += data
+    })
+    req.on("end", async() => {
+
+        const datos = Object.values(JSON.parse(body));
+
+        const algo = await insertar(datos[0], datos[1], datos[2])
+
+        res.status(201)
+
+        res.send(algo)
+    })
+})
+
+//Consultar
+app.get('/canciones', async(req, res) => {
+    let canciones = await consultar();
+    res.json(canciones);
+});
+
+//Actualizar
+app.put("/cancion", async(req, res) => {
+    let body = "";
+    req.on("data", (data) => {
+        body += data;
+    });
+
+    req.on("end", async() => {
+        const datos = Object.values(JSON.parse(body));
+        const act = await editar(Number(datos[0]), datos[1], datos[2], datos[3]);
+        res.status(201).json(act);
+    });
+});
+
+//Eliminar
+app.delete('/cancion', async(req, res) => {
+    await eliminar(req.query.id);
+
+    res.send('Cancion eliminada de forma satisfactoria')
+});
+
+app.listen(3000, () => console.log("Ejecutando el el puerto 3000"))
